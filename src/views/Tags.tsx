29991b792df20df37272bfdb24aa6922244096e7
dandanloveJM <gigi834921748@qq.com>
2020-12-06 19:37:35 +0800

    tags样式调整完成

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,15 +1,54 @@
 import * as React from 'react';
 import Layout from '../Components/Layout';
 import {useTags} from "../useTags";
+import Icon from "../Components/Icon";
+import styled from "styled-components";
+
+const TagList = styled.ol`
+  font-size: 16px;
+  background: white;
+  > li {
+    border-bottom: 1px solid #d5d5d9;
+    line-height: 20px;
+    padding: 12px 16px 12px 0;
+    margin-left: 16px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`
+const Button = styled.button`
+    font-size: 18px; border: none; padding: 8px 12px;
+    background: #f60; border-radius: 4px; color: white;
+`
+const Center = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`
+
+const Space = styled.div`
+    height: 16px;
+`
+
 
 const Tags: React.FunctionComponent = () => {
-    const {tags, setTags} = useTags()
+    const {tags} = useTags()
     return (
         <Layout>
-            <ol>
+            <TagList>
                 {tags.map(tag =>
-                    (<li key={tag}>{tag}</li>))}
-            </ol>
+                    (<li key={tag}>
+                        <span className="oneline">{tag}</span>
+                        <Icon name="right"/>
+                    </li>))}
+            </TagList>
+            <Center>
+                <Space/>
+                <Space/>
+                <Button>新增标签</Button>
+            </Center>
         </Layout>
     );
 };
