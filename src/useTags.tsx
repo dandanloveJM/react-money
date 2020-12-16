3b239ed3a8a1079100daf867f69a60b9bf810921
dandanloveJM <gigi834921748@qq.com>
2020-12-16 16:50:48 +0800

    新增updateTags

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -10,8 +10,29 @@ const defaultTags = [
 
 const useTags = () => {
     const [tags, setTags] = useState<{ id: number; name: string }[]>(defaultTags);
-    const findTags = (id: number) => tags.filter(tag => tag.id === id)[0]
-    return {tags, setTags, findTags}
+    const findTags = (id: number) => tags.filter(tag => tag.id === id)[0];
+    const findTagIndex = (id: number) => {
+        let result = -1
+        for (let i = 0; i < tags.length; i++) {
+            if (tags[i].id === id) {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+    // 传id是为了确定要改哪一个tag,
+    const updateTag = (id: number, obj: {name: string}) => {
+        const index = findTagIndex(id);
+        // 深拷贝tags
+        const newTags = JSON.parse(JSON.stringify(tags));
+        // 删掉原先的tag, 然后替换
+        newTags.splice(index, 1, {id, name: obj.name})
+        setTags(newTags);
+
+    }
+
+    return {tags, setTags, findTags, updateTag, findTagIndex}
 }
 
 export {useTags}
--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,20 +1,52 @@
 import React from "react";
 import {useTags} from "../useTags";
 import {useParams} from 'react-router-dom'
+import Layout from "../Components/Layout";
+import Icon from "../Components/Icon";
+import styled from "styled-components";
+import {Button} from "../Components/Button";
 
 type Params = {
     id: string
 }
 
+const TopBar = styled.header`
+ display: flex;
+ justify-content: space-between;
+ align-items: center;
+ line-height: 20px;
+ padding: 14px;
+ background: white;
+`
+
 const Tag: React.FunctionComponent = (props) => {
-    const {findTags} = useTags();
-    const {id} = useParams<Params>();
-    const tag = findTags(parseInt(id))
-    console.log(tag)
+    const {findTags, updateTag} = useTags();
+    const {id: idString} = useParams<Params>();
+    const tag = findTags(parseInt(idString))
     return (
-        <div>
-            {tag.name}
-        </div>
+       <Layout>
+           <TopBar>
+               <Icon name="left"/>
+               <span>编辑标签</span>
+               <Icon/>
+           </TopBar>
+           <div>
+               <label>
+                   <span>标签名</span>
+                   <input
+                          type="text"
+                          placeholder="标签名"
+                          value={tag.name}
+                          onChange={(e)=>{
+                              updateTag(tag.id, {name: e.target.value})
+                          }}
+                   />
+               </label>
+           </div>
+           <div>
+               <Button>删除标签</Button>
+           </div>
+       </Layout>
     )
 }
 
